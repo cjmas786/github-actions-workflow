d4eb1becfb1001dcd81f9f165588324d52fd9979
H200i <101483594+H200i@users.noreply.github.com>
2023-04-01 18:33:13 +0100

    Update main.tf

---
+++ Network/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2_sunny" {
+  ami           = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "net"
+  }
+}

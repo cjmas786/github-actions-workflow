aa13bb86897fca0066b7e8d4648dabc505f04888
H200i <101483594+H200i@users.noreply.github.com>
2023-04-01 17:41:47 +0100

    Update main.tf

--- DevTest/main.tf
+++ DevTest/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "my_ec2_fir" {
+resource "aws_instance" "my_ec2_sunny" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
   tags = {

53211aad7f3047e7187947e742edcebed635dee8
H200i <101483594+H200i@users.noreply.github.com>
2023-04-02 15:46:44 +0100

    Update main.tf

--- Network/main.tf
+++ Network/main.tf
@@ -19,3 +19,11 @@ resource "aws_instance" "my_ec2_sunny" {
     Name = "gen"
   }
 }
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "genX"
+  }
+}

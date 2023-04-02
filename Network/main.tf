8122e363349257ca05fb3779e490470e86a52768
H200i <101483594+H200i@users.noreply.github.com>
2023-04-02 06:08:07 +0100

    Update main.tf

--- Network/main.tf
+++ Network/main.tf
@@ -14,6 +14,6 @@ resource "aws_instance" "my_ec2_sunny" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
   tags = {
-    Name = "network-ec22"
+    Name = "network-ec223"
   }
 }

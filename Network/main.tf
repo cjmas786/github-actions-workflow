7a5d68ac04644ce6e4a400eb68c09d6e51a09a04
H200i <101483594+H200i@users.noreply.github.com>
2023-04-02 06:03:34 +0100

    Update main.tf

--- Network/main.tf
+++ Network/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "mys3sunny"
+    key    = "devtest/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_instance" "my_ec2_sunny" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
